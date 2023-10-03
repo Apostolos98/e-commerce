5233ced147b784ae86be18394c24d6751f164b3f
Apostolos98 <tolisbrasst@gmail.com>
2023-10-03 21:27:14 +0300

    first commit for / page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { useState, useEffect } from 'react'
-import Sidebar from './components/DropDown'
-import './styles/App.css'
+import styles from './styles/App.module.css'
+import { useNavigate } from 'react-router-dom'
 
 function App() {
+  const navigate = useNavigate();
   return (
-    <Sidebar />
+    <div className={styles.cont}>
+      <div className={styles.textImage}>
+        <img src="/src/assets/bestbuyApi.jpg" alt="" />
+        <div className={styles.text}>
+          <p className={styles.one}>This is my fake e-commeerce web site. It is based on Best Buy Api using most of the features the api provides to developers.</p>
+          <p>
+          The Best Buy API provides a simple, REST-based interface for an entire product catalog, past and present. 
+          This includes pricing, availability, specifications, descriptions, and images for more than one million current and historical products. 
+          Most product information is updated near real-time, including product pricing.
+          </p>
+        </div>
+      </div>
+      <button className={styles.button} onClick={() => navigate('/home')}>Shop Now</button>
+    </div>
   )
 }
 
